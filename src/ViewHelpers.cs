8ce01bb8ca7fc57c3f061a1b58ed50600d2a57f6
Luca Dalla Valle <lucadv91@gmail.com>
2019-02-09 12:52:32 +0100

    advancements

---
+++ src/ViewHelpers.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ldv.PersonalSite
+{
+    public static class ViewHelpers
+    {
+        public static int GetAge(DateTime birthdate)
+        {
+            // Save today's date.
+            var today = DateTime.Today;
+            // Calculate the age.
+            var age = today.Year - birthdate.Year;
+            // Go back to the year the person was born in case of a leap year
+            if (birthdate > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}
