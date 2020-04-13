099a673cd5f931f0fb95790a76de41b87fd3d93f
Luca Dalla Valle <luca.dallavalle@outlook.com>
2020-04-13 13:36:57 +0200

    implement fade functionality, fix timeline period string

---
+++ src/Pages/Index.cshtml.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ldv.PersonalSite.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IList<Experience> Experiences
+            => new List<Experience>()
+            {
+                new Experience()
+                {
+                    DateFrom = new DateTime(2019, 5, 1),
+                    DateTo = null,
+                    Company = "Texa Spa",
+                    Role = "Cloud Solutions Developer",
+                    CompanyUrl = new Uri("https://www.texa.com"), 
+                    Summary = "Developing Cloud Solutions in C# and .NET Core for Big Data and IoT architectures built on Microsoft Azure.",
+                    Accomplishments = new List<string>()
+                    {
+                        "Implemented a microservice solution to save the ingestion data into the main short-term database",
+                        "Developed a new cost-effective solution to store long-term Big Data",
+                        "Designed and implemented a highly parallelized set of Web APIs to query ingestion data from multiple data sources",
+                        "Developed a highly performant Web API for the realtime processing of geographical data",
+                        "Had a key role in the process of migrating the technology stack of the main short-term ingestion database",
+                        "Refactored and partially rewritten an Angular SPA for internal customers used for monitoring the cloud infrastructure",
+                    },
+                },
+                new Experience()
+                {
+                    DateFrom = new DateTime(2017, 11, 1),
+                    DateTo = new DateTime(2019, 4, 30),
+                    Role = "Full Stack Developer",
+                    Company = "Danea Soft",
+                    CompanyUrl = new Uri("https://www.danea.it"), 
+                    Summary = "Web Development using the Microsoft stack and standard web technologies.",
+                    Accomplishments = new List<string>()
+                    {
+                        "MioCondominio: single-handedly rewrote this core business app by converting it from a legacy ASP MPA to a SPA based on Angular 7+ and ASP.NET Core",
+                        "Agyo Console: designed and developed a MPA to expose some functionalities of the centralized TeamSystem invoicing platform to the end-users",
+                        "Authentication server: built an IdP based on the highest security standards to provide a centralized authentication platform for the different applications",
+                        "Domustudio: built the prototype of the next web-based version of the application using React and ASP.NET Core",
+                        "Had a critical role in the refactoring of the codebase by separating it into a three layer structure and by rewriting the DAL using Dapper",
+                        "Numilia: integrated the application with the Braintree platform",
+                    },
+                },
+                new Experience()
+                {
+                    DateFrom = new DateTime(2014, 9, 1),
+                    DateTo = new DateTime(2017, 10, 31),
+                    Role = "Developer",
+                    Company = "Maxima srl",
+                    CompanyUrl = new Uri("https://www.linkedin.com/company/maxima-s.r.l.l./"),
+                    Summary = "Customer support, development using VB6, SQL Server, VB.NET, C# and ASP.NET. Light project management.",
+                    Accomplishments = new List<string>()
+                    {
+                        "Learnt VB6",
+                        "Level 1 and 2 Help Desk customer phone support: troubleshooting, bugfixing, SQL Server data migrations",
+                        "Managed the startup of many new customers",
+                        ".NET 4.0+ development using VB.NET and C#",
+                        "OneStore: participated in the development of this new project using .NET Core 1.0, IdentityServer, Angular 4+, C#",
+                    },
+                },
+                new Experience()
+                {
+                    DateFrom = new DateTime(2013, 9, 1),
+                    DateTo = new DateTime(2014, 8, 31),
+                    Role = "Developer intern",
+                    Company = "Qfns Italy Srl",
+                    CompanyUrl = null,
+                    Summary = "Development using C#, .NET, Silverlight.",
+                    Accomplishments = new List<string>()
+                    {
+                        "Self-taught C# and the .NET Framework 4.6",
+                        "Participated actively in the development of the main business application",
+                        "Came in contact with DevOps best practices by practicing SCRUM diligently",
+                    },
+                },
+            };
+    }
+
+    public class Experience
+    {
+        public DateTime DateFrom { get; set; }
+        public DateTime? DateTo { get; set; }
+        public string Role { get; set; }
+        public string Company { get; set; }
+        public Uri? CompanyUrl { get; set; }
+        public string Summary { get; set; }
+        public List<string> Accomplishments { get; set; }
+    }
+}
--- src/wwwroot/js/site.js
+++ src/wwwroot/js/site.js
@@ -1,21 +1,65 @@
 let isArrowVisible = true;
-$(function () {
-    $("#ldv-arrow-container a").click(function (event) {
+
+function setupArrowEventHandlers() {
+    const $window = $(window),
+        $ldvarrow = $("#ldv-arrow-container a");
+
+    $ldvarrow.click(function (event) {
         event.preventDefault();
-        var viewportHeight = $(window).height();
+        var viewportHeight = $window.height();
 
         $('html, body').animate({
             scrollTop: viewportHeight
         }, 2000);
     });
-
-    $(window).scroll(function () {
-        if ($(window).scrollTop() === 0 && !isArrowVisible) {
+    $window.scroll(function () {
+        if ($window.scrollTop() === 0 && !isArrowVisible) {
             isArrowVisible = true;
-            $('#ldv-arrow-container').show();
+            $ldvarrow.show();
         } else if (isArrowVisible) {
             isArrowVisible = false;
-            $('#ldv-arrow-container').fadeOut();
+            $ldvarrow.fadeOut();
         }
     });
+}
+
+
+function setupFadeOnScrollEventHandlers() {
+    const $window = $(window);
+    let $allElementsToFade;
+    let $sectionsToFade;
+    let $timelineContainersToFade;
+
+    let getViewPortFadeLimit = function () {
+        return window.innerHeight - (window.innerHeight * 0.225);
+    };
+    
+    $sectionsToFade = $(".ldv-section .container")
+        .filter(function (a, b) {
+            if (b.parentElement.id !== "ldv-section-timeline") {
+                return b.getBoundingClientRect().top > getViewPortFadeLimit();
+            }
+        });
+    $timelineContainersToFade = $(".timeline-container")
+        .filter(function (a, b) {
+            return b.getBoundingClientRect().top > getViewPortFadeLimit();
+        });
+    
+    $allElementsToFade = $sectionsToFade.add($timelineContainersToFade);
+    $allElementsToFade.css("opacity", 0);
+    $window.scroll(function () {
+        $allElementsToFade.filter(function (a, b) {
+            return b.getBoundingClientRect().top < getViewPortFadeLimit();
+        })
+            .animate({opacity: 1}, 200);
+    });
+}
+
+
+
+// on document ready
+$(function () {
+    setupArrowEventHandlers();
+    setupFadeOnScrollEventHandlers();
 });
+
